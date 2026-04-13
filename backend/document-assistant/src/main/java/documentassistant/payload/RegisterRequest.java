16eb8872f13cd6d097db05dab3e9b105805e2c66
SlaVcE <slavce14.apps@gmail.com>
2026-04-13 19:09:15 +0200

    Require minimum 8-character password

--- backend/document-assistant/src/main/java/documentassistant/payload/RegisterRequest.java
+++ backend/document-assistant/src/main/java/documentassistant/payload/RegisterRequest.java
@@ -21,6 +21,6 @@ public class RegisterRequest {
     @Email(message = "Email should be valid")
     private String email;
     @NotBlank(message = "Password is required")
-//    @Size(min = 6, message = "Password must be at least 6 characters")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
